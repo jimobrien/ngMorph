e0f9d9d310e13b20ff469332b15aaa60dbc03f5e
Jim OBrien <jimobrien930@gmail.com>
2014-08-25 12:11:34 -0700

    set wrapper h/w

--- src/ngMorph.js
+++ src/ngMorph.js
@@ -13,17 +13,18 @@ angular.module('ngMorph', [])
       pre: function (scope, element, attrs) {
         // create wrapper. same size as origin element.
         // intialize event listener (get from config obj)
-
+        
         // get morphable height/width to pass to morph-wrapper directive
         scope.wrapperCfg = {
           height: element[0].offsetHeight,
-          width: element[0].offsetWidth
+          width: element[0].offsetWidth,
+          display: 'inline-block'
         };
 
         // compile wrapper directive, pass settings obj
         var wrapper = $compile('<morph-wrapper settings="wrapperCfg"/>')(scope);
         
-        // wrap morphable with morph-wrapper
+        // wrap morphable with morphWrapper
         element.wrap(wrapper);
       }
     }
@@ -34,20 +35,23 @@ angular.module('ngMorph', [])
     restrict: 'E',
     require: '^morphable', // share same instance of ctrl
     link: function (scope, element, attrs) {
-      
+      console.log(scope, 'into')
     }
   };
 }])
 .directive('morphWrapper', [function () {
   return {
     restrict: 'E',
-    
     scope: {
       settings: '='
     },
     link: function (scope, element, attrs) {
       // wrap the elements required for morphing effect
       // track state (morphed / normal)
+      console.log(scope);
+
+      // set height/width and normal-state styling
+      element.css(scope.settings);
     }
   };
 }]);
