6899a7e54c0d6ad4374b3aebb38c62a3dd3203fd
Jim OBrien <jimobrien930@gmail.com>
2014-08-25 12:41:45 -0700

    facilitate cross-directive communication

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 angular.module('morphDemo', ['ngMorph'])
 .controller('AppCtrl', ['$scope', function ($scope) {
 
+  $scope.morphSettings = {
+    trigger: 'click'
+  };
+
 }]);
--- src/ngMorph.js
+++ src/ngMorph.js
@@ -1,23 +1,46 @@
 angular.module('ngMorph', [])
-.controller('MorphCtrl', ['$scope', function ($scope) {
-  
+.controller('MorphCtrl', ['$scope', function ($scope, s, st) {
+  var ctrl = this;
+
+  ctrl.getOriginElementDimensions = function () {
+    return $scope.settings.originDimensions;
+  };
+
 }])
 .directive('morphable', ['$compile', function ($compile) {
+  var NormalStateStyles = {
+    'opacity': 1,
+    'z-index': 1000,
+    '-webkit-transition': 'opacity 0.1s 0.5s',
+    'transition': 'opacity 0.1s 0.5s',
+  };
+
+  var MorphedStateStyles = {
+    'z-index': 2000,
+    'opacity': 0,
+    '-webkit-transition': 'opacity 0.1s',
+    'transition': 'opacity 0.1s',
+    // 'pointer-events': 'none'
+  };
+
   return {
     restrict: 'A',
     controller: 'MorphCtrl',
     scope: {
-      morphable: '='
+      settings: '=morphable'
     },
     link: {
       pre: function (scope, element, attrs) {
-        // create wrapper. same size as origin element.
-        // intialize event listener (get from config obj)
-        
+        // initialize state
+        var state = scope.state = { isMorphed: false };
+        var settings = scope.settings;
+
+        settings.originDimensions = element[0].getBoundingClientRect();
+
         // get morphable height/width to pass to morph-wrapper directive
         scope.wrapperCfg = {
-          height: element[0].offsetHeight,
-          width: element[0].offsetWidth,
+          height: settings.originDimensions.height + 'px',
+          width: settings.originDimensions.width + 'px',
           display: 'inline-block'
         };
 
@@ -26,16 +49,33 @@ angular.module('ngMorph', [])
         
         // wrap morphable with morphWrapper
         element.wrap(wrapper);
+
+        // set normal state styles
+        element.css(NormalStateStyles);
+
+
+        // intialize event listener (get from config obj)
+        element.on(scope.settings.trigger, function () {
+          state.isMorphed ? element.css(NormalStateStyles) : element.css(MorphedStateStyles);
+          state.isMorphed = !state.isMorphed;
+        });
       }
     }
   };
 }])
-.directive('morphInto', [function () {
+.directive('morphInto', ['$compile', function ($compile) {
   return {
     restrict: 'E',
     require: '^morphable', // share same instance of ctrl
+    scope: {
+      template: '='
+    },
     link: function (scope, element, attrs) {
-      console.log(scope, 'into')
+      // compile scope.template
+      // set w/h to origin w/h
+      var template = $compile(scope.template)(scope);
+      console.log(template)
+      // var dimensions = ctrl.getOriginElementDimensions();      
     }
   };
 }])
@@ -48,7 +88,7 @@ angular.module('ngMorph', [])
     link: function (scope, element, attrs) {
       // wrap the elements required for morphing effect
       // track state (morphed / normal)
-      console.log(scope);
+      
 
       // set height/width and normal-state styling
       element.css(scope.settings);
