315a3d060aba98bccd0868c8eddb1bceaaab3b94
Jim OBrien <jimobrien930@gmail.com>
2014-08-25 11:43:46 -0700

    ngMorph module structure

--- src/ngMorph.js
+++ src/ngMorph.js
@@ -1,1 +1,25 @@
-angular.module('ngMorph', []);
+angular.module('ngMorph', [])
+.directive('morphable', [function () {
+  return {
+    restrict: 'A',
+    link: function (scope, iElement, iAttrs) {
+      
+    }
+  };
+}])
+.directive('morphInto', [function () {
+  return {
+    restrict: 'E',
+    link: function (scope, iElement, iAttrs) {
+      
+    }
+  };
+}])
+.directive('morphWrapper', [function () {
+  return {
+    restrict: 'A',
+    link: function (scope, iElement, iAttrs) {
+      
+    }
+  };
+}]);
