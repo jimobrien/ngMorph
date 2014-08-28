1b14bfc87d681753275b2f0cef49ff6b3c4345be
Jim OBrien <jimobrien930@gmail.com>
2014-08-27 20:09:01 -0700

    restructure prototype

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-angular.module('morphDemo', ['ngMorph'])
-.controller('AppCtrl', ['$scope', function ($scope) {
+angular.module('morphDemo', ['ngAnimate'])
 
+
+.controller('AppCtrl', ['$scope', function ($scope) {
   $scope.example1 = {
     trigger: 'click',
     closingEl: '.close-x',
@@ -8,7 +9,6 @@ angular.module('morphDemo', ['ngMorph'])
       url: 'views/loginform.html',
     }
   };
-
 
   $scope.example2 = {
     trigger: 'click',
@@ -19,18 +19,150 @@ angular.module('morphDemo', ['ngMorph'])
       height: '800px'
     }
   };
-
-
-  // Alertnate template method
-  // $scope.loginForm = '<div class="login" >' +
-  //                     '<span class="close"> X </span>' +
-  //                     '<form>' +
-  //                       '<p><label>Email</label><input type="text" /></p>' +
-  //                       '<p><label>Password</label><input type="password" /></p>' +
-  //                       '<p><button>Login</button></p>' +
-  //                     '</form>' +
-  //                   '</div>';
-
   
+}])
 
-}]);
+.animation('.ng-morphable-origin', function () {
+  return {
+    addClass: function (element, className) {
+
+    },
+    removeClass: function (element, className) {
+
+    }
+  };
+})
+
+
+
+.factory('NormalStateStyles', [function () {
+  return { 
+    wrapper: {
+      'visibility': 'hidden',
+      'position': 'fixed',
+      'z-index': '900',
+      'opacity': '0',
+      'margin': '0',
+    },
+    content: {
+      'visibility': 'hidden'
+    },
+    morphable: {
+      'z-index': '1000',
+      'outline': 'none',
+      '-webkit-transition': 'opacity 0.1s 0.5s',
+      'transition': 'opacity 0.1s 0.5s'
+    }
+  };
+}])
+
+
+.factory('MorphedStateStyles', [function () {
+
+  return { 
+    Wrapper: {
+      'visibility': 'hidden'
+    },
+    Content: {
+      'visibility': 'hidden'
+    }
+  };
+}])
+
+
+
+.factory('Morph', ['$animate', 'NormalStateStyles', 'MorphedStateStyles', function ($animate, NormalStateStyles, MorphedStateStyles) {
+
+  function initialize (morphable, content, settings) {
+    var MorphableBoundingRect;
+    var ContentBoundingRect;
+
+    this.wrapper   = wrapper = angular.element('<div></div>').css('visibility', 'hidden');
+    this.content   = content;
+    this.morphable = morphable;
+
+    // add to dom
+    wrapper.append(content);
+    morphable.after(wrapper);
+
+    // get content dimensions
+    MorphableBoundingRect = morphable[0].getBoundingClientRect();
+    ContentBoundingRect   = content[0].getBoundingClientRect();
+    
+    // set wrapper position
+    wrapper.css({
+      'top': MorphableBoundingRect.top + 'px',
+      'left': MorphableBoundingRect.left + 'px',
+      'width': MorphableBoundingRect.width + 'px',
+      'height': MorphableBoundingRect.height + 'px'
+    });
+
+    // apply normal-state styles
+    morphable.css(NormalStateStyles.morphable);
+    wrapper.css(NormalStateStyles.wrapper);
+    content.css(NormalStateStyles.content);
+
+    // init event handlers for morphable 
+  }
+
+  function initEvents () {
+
+  }
+
+  return {
+    initialize: initialize
+  };
+}])
+
+
+
+.directive('ngMorphable', ['$compile', '$http', 'Morph', function ($compile, $http, Morph) {
+  // function initialize (morphable, compiledContent, settings) {
+  //   Morph.initialize(morphable, compiledContent, settings);
+  // }
+
+  var postLinking = function (scope, element, attrs) {
+    var fetchContent = $http.get(scope.settings.template.url);
+
+    var compileContent = function (results) {
+      if ( results ) scope.morphTemplate = results.data;
+
+      return $compile(scope.morphTemplate)(scope);    
+    };
+
+    fetchContent.then(compileContent)
+    .then( function (compiledContent) {
+      Morph.initialize(element, compiledContent, scope.settings);
+      // Morph.content
+      // Morph.wrappedContent
+    });
+  };
+
+  return {
+    restrict: 'A',
+    scope: {
+      // morphTemplate: '=',
+      settings: '=ngMorphable'
+    },
+    link: postLinking
+  };
+}])
+
+
+
+
+.directive('ngMorphInto', ['$compile', function ($compile) {
+  return {
+    restrict: 'E',
+    scope: false, // sibling scope, just being explicit
+    template: '<div></div>',
+    replace: true,
+    link: function (scope, element, attrs) {
+      var content = $compile(attrs.template)(scope);
+      element.append(content);
+      
+    }
+  }; 
+}])
+
+;
