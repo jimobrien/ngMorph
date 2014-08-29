e9ca7365129d476b09a9e9a7a2466873046e98b8
Jim OBrien <jimobrien930@gmail.com>
2014-08-29 16:26:49 -0700

    add  handler

--- src/directives/modal.js
+++ src/directives/modal.js
@@ -1,66 +1,6 @@
 angular.module('morph.directives')
 .directive('ngMorphModal', ['$http', '$templateCache', '$compile', 'Morph', function ($http, $templateCache, $compile, Morph) {
   var isMorphed = false;
-  
-  var postLinking = function (scope, element, attrs) {
-    var loadContent = $http.get(scope.settings.template.url, { cache: $templateCache });
-
-    var compile = function (results) {
-      if ( results ) scope.morphTemplate = results.data;
-
-      return $compile(scope.morphTemplate)(scope);    
-    };
-
-    loadContent.then(compile)
-    .then( function (content) {
-      var wrapper = angular.element('<div></div>').css('visibility', 'hidden');
-      var closeEl = angular.element(content[0].querySelector(scope.settings.closeEl));
-      var elements = {
-        morphable: element,
-        wrapper: wrapper,
-        content: content
-      };
-
-      // add to dom
-      wrapper.append(content);
-      element.after(wrapper);
-
-      // get bounding rectangles
-      scope.settings.MorphableBoundingRect = element[0].getBoundingClientRect();
-      scope.settings.ContentBoundingRect = content[0].getBoundingClientRect();
-
-      // bootstrap modal
-      var modal = Morph.modal(elements, scope.settings);
-
-      // attach event listeners
-      element.bind('click', function () {
-        if ( isMorphed ) { // testing only
-          angular.forEach(elements, function (element, elementName) {
-            modal.removeClass(element, elementName, scope.settings);
-          });
-        } else {
-          angular.forEach(elements, function (element, elementName) {
-            modal.addClass(element, elementName, scope.settings);
-          });
-        }
-
-        isMorphed = !isMorphed;
-      });
-
-      closeEl.bind('click', function (event) {
-        if ( isMorphed ) {
-          
-          angular.forEach(elements, function (element, elementName) {
-            modal.removeClass(element, elementName, scope.settings);
-          });
-
-          isMorphed = !isMorphed;
-        }
-      });
-      
-    });
-
-  };
 
   return {
     restrict: 'A',
@@ -68,6 +8,70 @@ angular.module('morph.directives')
       // morphTemplate: '=',
       settings: '=ngMorphModal'
     },
-    link: postLinking
+    link: function (scope, element, attrs) {
+      var loadContent = $http.get(scope.settings.template.url, { cache: $templateCache });
+
+      var compile = function (results) {
+        if ( results ) scope.morphTemplate = results.data;
+
+        return $compile(scope.morphTemplate)(scope);    
+      };
+
+      loadContent.then(compile)
+      .then( function (content) {
+        var wrapper = angular.element('<div></div>').css('visibility', 'hidden');
+        var closeEl = angular.element(content[0].querySelector(scope.settings.closeEl));
+        var elements = {
+          morphable: element,
+          wrapper: wrapper,
+          content: content
+        };
+
+        // add to dom
+        wrapper.append(content);
+        element.after(wrapper);
+
+        // get bounding rectangles
+        scope.settings.MorphableBoundingRect = element[0].getBoundingClientRect();
+        scope.settings.ContentBoundingRect = content[0].getBoundingClientRect();
+
+        // bootstrap modal
+        var modal = Morph.modal(elements, scope.settings);
+        
+        // attach event listeners
+        element.bind('click', function () {
+          if ( isMorphed ) { // testing only
+            angular.forEach(elements, function (element, elementName) {
+              modal.removeClass(element, elementName, scope.settings);
+            });
+          } else {
+            angular.forEach(elements, function (element, elementName) {
+              modal.addClass(element, elementName, scope.settings);
+            });
+          }
+
+          isMorphed = !isMorphed;
+        });
+
+        if ( closeEl ) {
+          closeEl.bind('click', function (event) {
+            if ( isMorphed ) {
+              
+              angular.forEach(elements, function (element, elementName) {
+                modal.removeClass(element, elementName, scope.settings);
+              });
+
+              isMorphed = !isMorphed;
+            }
+          });
+        }
+
+        scope.$on('$destroy', function () {
+          element.unbind('click');
+          closeEl.unbind('click');
+        });
+      });
+
+    }
   };
 }]);
