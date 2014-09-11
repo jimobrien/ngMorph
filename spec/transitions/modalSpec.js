87f6bd3d47867e015e0a16ba432ae450963f379c
loveky <ylzcylx@gmail.com>
2014-09-11 22:59:09 +0800

    add spec for modal directive

--- spec/transitions/modalSpec.js
+++ spec/transitions/modalSpec.js
@@ -55,4 +55,52 @@ describe('Modal Transition', function(){
     });
   });
 
+  it('should open modal when directive element is clicked', function() {
+    inject(function ($compile, $document, $rootScope, $rootElement) {
+      var morphable = $compile('<div><button ng-morph-modal="settings"> Test </button></div>')($rootScope);
+
+      $rootElement.append(morphable);
+      angular.element($document[0].body).append($rootElement);
+      runs(function() {
+        morphable.find("button")[0].click();
+      });
+
+      waitsFor(function() {
+        return getComputedStyle(morphable.find("div")[0], null).opacity === "1";
+      }, "wrapper should be visible to user", 35);
+    });
+  });
+
+  it('should close modal when closeEl is clicked', function() {
+    inject(function ($compile, $document, $rootScope, $rootElement) {
+      var tplWithCloseEl = '<div width="width:400px; height: 400px; background:black;display:block;"> <span class="close-x">x</span> <h1> Test Content </h1> </div>';
+      $rootScope.settings = {
+        closeEl: ".close-x",
+        trigger: 'click',
+        modal: {
+          template: tplWithCloseEl
+        }
+      };
+
+      var morphable = $compile('<div><button ng-morph-modal="settings"> Test </button></div>')($rootScope);
+
+      $rootElement.append(morphable);
+      angular.element($document[0].body).append($rootElement);
+      runs(function() {
+        morphable.find("button")[0].click();
+      });
+
+      waitsFor(function() {
+        return getComputedStyle(morphable.find("div")[0], null).visibility === "visible";
+      }, "wrapper should be visible to user", 35);
+
+      runs(function() {
+        morphable.children(".close-x")[0].click();
+      });
+
+      waitsFor(function() {
+        return getComputedStyle(morphable.find("div")[0], null).opacity === '0' ;
+      }, "wrapper should be hidden to user", 850);
+    });
+  });
 });
